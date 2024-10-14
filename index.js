2f0942010f936c8c0016316cef43ab6653918bf7
Muxsimov <muxsimovasadbek665@gmail.com>
2024-10-14 14:23:15 +0500

    finished

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let olma=prompt("olma narxini kititing");
+let ananas=prompt("ananas narxini kititing");
+let xurmo=prompt("xurmo narxini kititing");
+let qulupnay=prompt("qulupnay narxini kititing");
+let banan=prompt("banan narxini kititing");
+
+let sum=parseInt(olma)+parseInt(ananas)+parseInt(xurmo)+parseInt(qulupnay)+parseInt(banan);
+
+alert(sum);
